adc716bceffd1145ebc18042814ed6fa09bc2e34
Sofia Barynova <sofiabarynova@gmail.com>
2025-04-21 08:57:13 +0300

    Lesson 3

---
+++ src/main/java/ua/pro/baynova/lessons/lesson_3/Main.java
@@ -0,0 +1,22 @@
+package ua.pro.baynova.lessons.lesson_3;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleHashSet<String> set = new SimpleHashSet<>();
+
+        System.out.println(set.add("apple"));
+        System.out.println(set.add("banana"));
+        System.out.println(set.add("apple"));
+
+        System.out.println(set.contains("banana"));
+        System.out.println(set.contains("grape"));
+
+        System.out.println(set.size());
+        System.out.println(set);
+
+        System.out.println(set.remove("banana"));
+        System.out.println(set.remove("grape"));
+
+        System.out.println(set);
+    }
+}
