8ffc618ce077d306fc25100d66b8ab38a50a3a91
Sofia Barynova <sofiabarynova@gmail.com>
2025-04-29 10:58:19 +0300

    Lesson 10 (Random numbers)

--- src/main/java/ua/pro/baynova/lessons/lesson_10/QuickSortSimple.java
+++ src/main/java/ua/pro/baynova/lessons/lesson_10/QuickSortSimple.java
@@ -1,30 +1,41 @@
 package ua.pro.baynova.lessons.lesson_10;
 
+import java.util.Random;
+
 public class QuickSortSimple {
     public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random rand = new Random();
 
-        int[] arr = {3, 5, 2, 9, 7, 4, 10};
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(100);
+
+        }
+
+        System.out.println("До сортировки:");
+        printArray(arr);
+
         quickSort(arr, 0, arr.length - 1);
 
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
+        System.out.println("После сортировки:");
+        printArray(arr);
+
     }
 
     private static void quickSort(int[] arr, int left, int right) {
         if (left >= right) return;
 
         int pivot = arr[right];
-
         int wall = left;
+
         for (int i = left; i < right; i++) {
             if (arr[i] < pivot){
                 swap(arr, i, wall);
                 wall++;
             }
         }
-        swap(arr, wall, right);
 
+        swap(arr, wall, right);
         quickSort(arr , left , wall - 1);
         quickSort(arr, wall + 1, right);
     }
@@ -34,5 +45,10 @@ public class QuickSortSimple {
         arr[i] = arr[j];
         arr[j] = tmp;
 
+    }
+
+    private static void printArray(int[] arr) {
+        for (int num : arr) System.out.print(num + " ");
+        System.out.println();
     }
 }
