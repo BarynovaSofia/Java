95785628821124f039f0045089f4b05564c260f6
Sofia Barynova <sofiabarynova@gmail.com>
2025-04-30 20:31:20 +0300

    Lesson 12

---
+++ src/main/java/ua/pro/baynova/lessons/lesson_12/Order.java
@@ -0,0 +1,15 @@
+package ua.pro.baynova.lessons.lesson_12;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Order {
+    int id;
+    String customerName;
+
+    public Order(int id, String customerName) {
+        this.id = id;
+        this.customerName = customerName;
+    }
+}
