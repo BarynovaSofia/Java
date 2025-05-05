91343362574bfa0191757bdb49d211bcb0745cab
Sofia Barynova <sofiabarynova@gmail.com>
2025-05-05 12:48:26 +0300

    JUnit Example (lesson 1)

--- src/test/java/ua/pro/baynova/JUnitExamle/CalculatorTest.java
+++ src/test/java/ua/pro/baynova/JUnitExamle/CalculatorTest.java
@@ -1,12 +1,22 @@
 package ua.pro.baynova.JUnitExamle;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculatorTest extends TestCase {
+public class CalculatorTest {
 
-    public void testAdd() {
-        Calculator calc = new Calculator();
+    Calculator calc = new Calculator();
+
+    @Test
+    void testAdd() {
         int result = calc.add(10, 12);
         assertEquals(22, result);
+    }
+
+    @Test
+    void testSquare() {
+        assertEquals(25, calc.square(5));
+        assertEquals(0, calc.square(0));
+        assertEquals(16, calc.square(-4));
     }
 }
